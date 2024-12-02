ff57e55be7e96810eb53e8da547b87058c839c53
Chris Lam <clam50@stuy.edu>
2024-12-02 15:35:21 -0500

    temp

--- shell.c
+++ shell.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <time.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <dirent.h>
+#include <signal.h>
 
-int main(){
-  
+static void sighandler(int signo) {
+
+}
+
+int main(int argc, char *argv[]){
+  signal(SIGQUIT, sighandler);
+
 }
