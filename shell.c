faa1c69a6ed6d2d7beefba0b28b931c63f6ad1da
Ivan Gontchar <ivan.tardis@outlook.com>
2024-12-03 15:10:11 -0500

    prompt display works but is pretty long

--- shell.c
+++ shell.c
@@ -13,32 +13,43 @@
 
 int main(int argc, char *argv[]){
   char line_buff[256];
+
+  char loc[1024];
+  getcwd(loc, 1024);
+  printf("%s/ $ ", loc);
+  fflush(stdout);
+
   while(fgets(line_buff,255,stdin)){
-	char *nl = strchr(line_buff, '\n');
-	*nl = '\0';
-	char *args[256];
-	char a[256];
-	strcpy(a, line_buff);
-	char *reala = a;
-	char *token;
-	int count= 0;
-	while((token = strsep(&reala," "))!= NULL){
-	 args[count] = token;
-	 count++;
-	}
-	args[count] = NULL;
-	pid_t p;
-	p = fork();
-	if(p < 0){
-     printf("%s\n",strerror(errno));
+    char loc[1024];
+    getcwd(loc, 1024);
+    printf("%s/ $ ", loc);
+    fflush(stdout);
+
+  	char *nl = strchr(line_buff, '\n');
+  	*nl = '\0';
+  	char *args[256];
+  	char a[256];
+  	strcpy(a, line_buff);
+  	char *reala = a;
+  	char *token;
+  	int count= 0;
+  	while((token = strsep(&reala," "))!= NULL){
+  	 args[count] = token;
+  	 count++;
+  	}
+  	args[count] = NULL;
+  	pid_t p;
+  	p = fork();
+  	if(p < 0){
+       printf("%s\n",strerror(errno));
+      }
+      else if(p == 0){
+  	  execvp(args[0], args);
+  	  exit(0);
+      }
+      int status;
+      wait(&status);
     }
-    else if(p == 0){
-	  execvp(args[0], args);
-	  exit(0);
-    }
-    int status;
-    wait(&status);
-  }
   exit(0);
- 
+
 }
