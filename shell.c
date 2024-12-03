f2de2ea15f06cbb282cc32d27665b1af089b1ccf
Chris Lam <clam50@stuy.edu>
2024-12-02 21:51:35 -0500

    utterly lost

--- shell.c
+++ shell.c
@@ -7,14 +7,37 @@
 #include <errno.h>
 #include <time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <dirent.h>
 #include <signal.h>
 
-static void sighandler(int signo) {
-
-}
-
 int main(int argc, char *argv[]){
-  signal(SIGQUIT, sighandler);
-
+  char line_buff[256];
+  while(fgets(line_buff,255,stdin)){
+	char *args[256];
+	char a[256];
+	strcpy(a, line_buff);
+	char *reala = a;
+	char *token;
+	int count= 0;
+	while((token = strsep(&reala," "))!= NULL){
+	 args[count] = token;
+	 count++;
+	}
+	args[count] = NULL;
+	pid_t p;
+	p = fork();
+	if(p < 0){
+     printf("%s\n",strerror(errno));
+    }
+    else if(p == 0){
+	  printf("%s\n", line_buff);
+	  execvp(args[0], args);
+	  exit(0);
+    }
+    int status;
+    wait(&status);
+  }
+  exit(0);
+ 
 }
